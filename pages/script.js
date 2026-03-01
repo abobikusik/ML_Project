deafcc221819e9b0809d5fe6d0f1d4cc415e9c9b
Larionova Irina <larionovaid@mer.ci.nsu.ru>
2026-03-01 18:52:06 +0700

    empty forms without JS logic

--- pages/script.js
+++ pages/script.js
@@ -1,18 +1,2 @@
-function selectCategory(category, element) {
-    
-    // Сбрасываем цвета у всех кнопок
-    let buttons = document.querySelectorAll('.category-btn');
-    buttons.forEach(btn => {
-        btn.style.background = '#f8f9fa';
-        btn.style.color = '#495057';
-    });
-    
-    // Подсвечиваем выбранную кнопку
-    if (element) {
-        element.style.background = '#0066cc';
-        element.style.color = 'white';
-    }
-}
-
 // Для проверки что js загрузился
-console.log('JS работает! 🐒');
+console.log('JS работает!');
