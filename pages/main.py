40b7dd46ba2ac9f3ee8cdd5b80bb63d912986e30
Larionova Irina <larionovaid@mer.ci.nsu.ru>
2026-03-01 23:56:04 +0700

    an example of data acquisition and navigation

---
+++ pages/main.py
@@ -0,0 +1,55 @@
+
+from sqlalchemy.orm import Session
+from fastapi import Depends, FastAPI, Body
+from fastapi.responses import JSONResponse, FileResponse
+from fastapi.staticfiles import StaticFiles
+  
+app = FastAPI()
+
+#Подключение CSS и JS
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+def main():
+    return FileResponse("index.html")
+
+#Для навигации по сайту
+@app.get("/phone_form")
+def get_phone_form():
+    return FileResponse("phone_form.html")
+
+@app.get("/laptop_form")
+def get_laptop_form():
+    return FileResponse("laptop_form.html")
+
+@app.get("/tv_form")
+def get_tv_form():
+    return FileResponse("tv_form.html")
+
+#Проверка получения данных после отправки формы
+@app.post("/phone_form")
+def print_phone_data(data = Body()):
+    a1 = data["brand"]
+    a2 = data["model"]
+    a3 = data["screen_size"]
+    a4 = data["display_type"]
+    a5 = data["screen_refresh"]
+    print(f"{a1}, {a2}, {a3}, {a4}, {a5}")
+
+@app.post("/laptop_form")
+def print_laptop_data(data = Body()):
+    a1 = data["brand"]
+    a2 = data["model"]
+    a3 = data["screen_size"]
+    a4 = data["display_type"]
+    a5 = data["screen_refresh"]
+    print(f"{a1}, {a2}, {a3}, {a4}, {a5}")
+
+@app.post("/tv_form")
+def print_tv_data(data = Body()):
+    a1 = data["brand"]
+    a2 = data["model"]
+    a3 = data["screen_size"]
+    a4 = data["display_type"]
+    a5 = data["screen_refresh"]
+    return  print(f"{a1}, {a2}, {a3}, {a4}, {a5}")
