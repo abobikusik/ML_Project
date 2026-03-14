ca660428c6186e4d28323c463a86d7e297a09c14
Larionova Irina <larionovaid@mer.ci.nsu.ru>
2026-03-14 21:20:06 +0700

    updated backend

--- my-app/backend/database_folder/db_pure_sql/main.py
+++ my-app/backend/database_folder/db_pure_sql/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles
 import logging
 import uvicorn
 from database import router
