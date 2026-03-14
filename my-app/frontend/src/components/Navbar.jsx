c9bfcfdfbd2a756723b1cb02c25c4f2b7dbf10b2
Larionova Irina <larionovaid@mer.ci.nsu.ru>
2026-03-14 21:23:00 +0700

    history page on react

--- my-app/frontend/src/components/Navbar.jsx
+++ my-app/frontend/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-const NavbarCustom = ({ onNavClick }) => {
+const NavbarCustom = ({ onNavClick, currentPage }) => {
   return (
     <nav className="navbar navbar-expand-lg navbar-custom">
       <div className="container-fluid">
@@ -18,7 +18,9 @@ const NavbarCustom = ({ onNavClick }) => {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" onClick={() => onNavClick("home")}>
+              <a 
+                className={`nav-link ${currentPage === "home" ? "active" : ""}`} 
+                onClick={() => onNavClick("home")}>
                 🏠 Главная
               </a>
             </li>
@@ -51,7 +53,10 @@ const NavbarCustom = ({ onNavClick }) => {
                   </a>
                 </li>
                 <li>
-                  <a className="dropdown-item" onClick={() => onNavClick("tv")}>
+                  <a 
+                    className="dropdown-item" 
+                    onClick={() => onNavClick("tv")}
+                  >
                     📺 Телевизоры
                   </a>
                 </li>
@@ -60,8 +65,8 @@ const NavbarCustom = ({ onNavClick }) => {
 
             <li className="nav-item">
               <a
-                className="nav-link"
-                onClick={() => console.log("история запросов")}
+                className={`nav-link ${currentPage === "history" ? "active" : ""}`}
+                onClick={() => onNavClick("history")}
               >
                 📝 История запросов
               </a>
